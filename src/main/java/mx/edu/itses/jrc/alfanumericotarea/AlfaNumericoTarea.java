5f65389aeb3191c2447158b9bd9302931d284ef4
Josue Caballero <l.rocj041012@itses.edu.mx>
2025-04-11 15:24:00 -0600

    Tarea de los 4 hilos

--- src/main/java/mx/edu/itses/jrc/alfanumericotarea/AlfaNumericoTarea.java
+++ src/main/java/mx/edu/itses/jrc/alfanumericotarea/AlfaNumericoTarea.java
@@ -3,14 +3,65 @@
  */
 
 package mx.edu.itses.jrc.alfanumericotarea;
-
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import mx.edu.itses.jrc.alfanumericotarea.Tipo;
 /**
  *
  * @author juras
  */
-public class AlfaNumericoTarea {
+
+
+public class AlfaNumericoTarea implements Runnable {
+
+    private Tipo tipo;
+
+    public AlfaNumericoTarea(Tipo tipo) {
+        this.tipo = tipo;
+    }
+
+  
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // Tarea 1 Thread clásico usando Runnable 
+        Thread hilo1 = new Thread(() -> 
+        new AlfaNumericoTarea(Tipo.NUMERO).run(), "Thread-Normal");
+        hilo1.start();
+
+        //  Tarea 2 ExecutorService 
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        executor.execute(() -> new AlfaNumericoTarea(Tipo.LETRA).run());
+        executor.shutdown();
+
+        // Tarea 3 Runnable
+        Runnable tarea3 = () -> new AlfaNumericoTarea(Tipo.NUMERO).run();
+        new Thread(tarea3, "Thread-Lambda").start();
+
+        // 4.Tarea 4 TimerTask
+        Timer timer = new Timer("Timer-Hilo");
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                new AlfaNumericoTarea(Tipo.LETRA).run();
+            }
+        }, 2000);
+
+        System.out.println("Hilos lanzados desde el main");
+    }
+    
+      @Override
+    public void run() {
+        if (tipo == Tipo.NUMERO) {
+            for (int i = 1; i < 10; i++) {
+                System.out.println(Thread.currentThread().getName() + " Número: " + i);
+            }
+        } else if (tipo == Tipo.LETRA) {
+            for (char c = 'A'; c <= 'Z'; c++) {
+                System.out.println(Thread.currentThread().getName() + " Letra: " + c);
+            }
+        }
     }
 }
---
+++ src/main/java/mx/edu/itses/jrc/alfanumericotarea/Tipo.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.edu.itses.jrc.alfanumericotarea;
+
+/**
+ *
+ * @author juras
+ */
+public enum Tipo{
+ NUMERO, LETRA;
+}
