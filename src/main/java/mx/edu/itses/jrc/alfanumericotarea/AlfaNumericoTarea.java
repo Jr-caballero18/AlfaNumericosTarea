186e7f242dedca506f7987a4b56a32461ef654e2
Josue Caballero <l.rocj041012@itses.edu.mx>
2025-04-11 09:16:15 -0600

    push del 11/04/25

---
+++ src/main/java/mx/edu/itses/jrc/alfanumericotarea/AlfaNumericoTarea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mx.edu.itses.jrc.alfanumericotarea;
+
+/**
+ *
+ * @author juras
+ */
+public class AlfaNumericoTarea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
